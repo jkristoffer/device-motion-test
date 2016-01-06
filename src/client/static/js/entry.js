10c1cd22f4fa3cdb5143f344581c13cbd7c17795
jkris <cattails27@gmail.com>
2016-01-06 09:25:10 +0800

    phone to browser

--- src/client/static/js/entry.js
+++ src/client/static/js/entry.js
@@ -1,23 +1,4 @@
 window.socket_1 = new WebSocket("ws://10.4.1.28:4001");
 
-var ReactDOM = window.ReactDOM = require('react-dom');
-var React = window.React = require('react');
-
-var controller = {
-	sendHi: function(){
-		socket_1.send({ message: 'hi' });
-	}
-};
-
-
-window.socket_1.onmessage = function(data){
-	console.log(data);
-	controller.receiveHi();
-};
-
-var Main = require('./components/main.jsx')(controller);
-ReactDOM.render(React.createElement(Main, null), document.getElementById('app'));
-
-/** Require Styles */
-
-require('../css/style.css');
+var playfield = require('./playfield.js');
+playfield.init(socket_1);
